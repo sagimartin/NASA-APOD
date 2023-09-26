ba19f3e56c4f88c6066c3c60df2ba06790390f5d
Martin Sági <sagimrtin@gmail.com>
2023-09-26 22:04:50 +0200

    Fix NavBar style

--- NASA APOD/src/components/NavBar.jsx
+++ NASA APOD/src/components/NavBar.jsx
@@ -1,7 +1,9 @@
 export default function NavBar() {
     return (
+        <div className="navBar">
         <div className="divider">
             NASA Astronomy Picture of the Day
+        </div>
         </div>
     )
 }
